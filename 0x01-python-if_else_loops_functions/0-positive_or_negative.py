4279caadb1b5f33ddc6a35cb64e0932346693810
mosesobeh1 <mosesobeh@gmail.com>
2023-09-17 09:21:51 -0700

    control structures

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number == True:
+    print(f"{number} is positive")
+elif number == false:
+    print(f"{number} is negative")
+else:
+    print(f"{number} is zero")
